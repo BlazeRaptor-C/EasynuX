da34fff4636cbc7efa527bf789f4f55aa8ff61e3
BlazeRaptor-C <79918931+BlazeRaptor-C@users.noreply.github.com>
2021-06-02 01:34:36 -0300

    Adaptação para uso do BackEnd atualizado
    Realizado pequenas mudanças na estilização da página obter + atualização dos códigos javascript para consumo do backend melhorado

--- EasynuX-Site/Scripts/index.js
+++ EasynuX-Site/Scripts/index.js
@@ -2,9 +2,9 @@ import api from '/Scripts/api.js';
 
 async function socolector() {
   let solist = []
-  const data = await api.read('hosts');
+  const data = await api.read('oslistjson');
   for (let cnt = 0; cnt <= data.length -1; cnt++) {
-    solist.push(JSON.stringify(data[cnt]['sistema'])+'\n')
+    solist.push(JSON.stringify(data[cnt]['sistema'])+'  -->  '+JSON.stringify(data[cnt]['status'])+'\n')
   }
   return document.getElementById("socolectorRETRN").textContent = solist;
 }
